31ad11eeca7171f617167beea16955875a69e71d
OEmiliatanO <emiliaistruelove@gmail.com>
2022-10-07 01:09:49 +0800

    completed all the required image operation. (auto-level, bit-slicing, average filter, sharpen filter, median filter, Laplacian filter)

--- hw2/imageop.py
+++ hw2/imageop.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 def chctrLINEAR(img, a = 1, b = 0):
 	if img == None:
-		print("error occur in chctr.py:chctrLINEAR(img, a, b):")
+		print("error occur in imageop.py:chctrLINEAR(img, a, b):")
 		print("img is None.")
 		return None
 
 	row = img.size[0]
 	col = img.size[1]
 	resIMG = Image.new("L", (row, col))
-	for x in range(1, row):
-		for y in range(1, col):
+	for x in range(row):
+		for y in range(col):
 			val = a * img.getpixel((x, y)) + b
 			if val >= 255:
 				val = 255
@@ -21,15 +21,15 @@ def chctrLINEAR(img, a = 1, b = 0):
 
 def chctrEXPON(img, a = 1, b = 0):
 	if img == None:
-		print("error occur in chctr.py:chctrEXPON(img, a, b):")
+		print("error occur in imageop.py:chctrEXPON(img, a, b):")
 		print("img is None.")
 		return None
 
 	row = img.size[0]
 	col = img.size[1]
 	resIMG = Image.new("L", (row, col))
-	for x in range(1, row):
-		for y in range(1, col):
+	for x in range(row):
+		for y in range(col):
 			try:
 				#print(img.getpixel((x, y)), end = ' ')
 				val = math.exp(a * img.getpixel((x, y)) + b)
@@ -43,19 +43,19 @@ def chctrEXPON(img, a = 1, b = 0):
 
 def chctrLOG(img, a = 1, b = 0):
 	if img == None:
-		print("error occur in chctr.py:chctrLOG(img, a, b):")
+		print("error occur in imageop.py:chctrLOG(img, a, b):")
 		print("img is None.")
 		return None
 	if b <= 1:
-		print("error occur in chctr.py:chctrLOG(img, a, b):")
+		print("error occur in imageop.py:chctrLOG(img, a, b):")
 		print("b must be bigger 1.")
 		return img
 	
 	row = img.size[0]
 	col = img.size[1]
 	resIMG = Image.new("L", (row, col))
-	for x in range(1, row):
-		for y in range(1, col):
+	for x in range(row):
+		for y in range(col):
 			val = math.log(a * img.getpixel((x, y)) + b)
 			if val >= 255:
 				val = 255
@@ -64,18 +64,18 @@ def chctrLOG(img, a = 1, b = 0):
 
 def grayhhlight(img, lb = 0, ub = 255, hhlightV = 255, preserve = True):
 	if img == None:
-		print("error occurs in grayslice.py:grayhhlight(img, lb, ub, preserve):")
+		print("error occurs in imageop.py:grayhhlight(img, lb, ub, preserve):")
 		print("img is None.")
 		return None
 	if lb > ub:
-		print("error occurs in grayslice.py:grayhhlight(img, lb, ub, preserve):")
+		print("error occurs in imageop.py:grayhhlight(img, lb, ub, preserve):")
 		print("lower bound is bigger than upper bound.")
 		return img
 	row = img.size[0]
 	col = img.size[1]
 	resIMG = Image.new("L", (row, col))
-	for x in range(1, row):
-		for y in range(1, col):
+	for x in range(row):
+		for y in range(col):
 			val = img.getpixel((x, y))
 			if val >= lb and val <= ub:
 				val = hhlightV
@@ -86,7 +86,7 @@ def grayhhlight(img, lb = 0, ub = 255, hhlightV = 255, preserve = True):
 
 def negative(img):
 	if img == None:
-		print("error occurs in negative.py:negative:")
+		print("error occurs in imageop.py:negative:")
 		print("img is None")
 		return None
 	row = img.size[0]
@@ -128,19 +128,118 @@ def ravel(img):
 	return arr
 
 def extract_bit(img, i):
-	pass
+	if i >= 8 or i < 0:
+		print("error occurs in imageop.py:extract_bit:")
+		print("the selected bit is out of range. selected bit:", i, ", but the vaild range is [0, 7].")
+		return None
+	if img == None:
+		print("error occurs in imageop.py:extract_bit:")
+		print("img is None")
+		return None
+	bmask = [255 & (1 << j) for j in range(8)]
+	row = img.size[0]
+	col = img.size[1]
+	resIMG = Image.new("L", (row, col))
+	for x in range(row):
+		for y in range(col):
+			val = int(img.getpixel((x, y)))
+			resIMG.putpixel((x, y), val & bmask[i])
+	return resIMG
 
-def smooth(img, rang):
-	pass
+def average_filter(img, rang = 3):
+	if not (rang & 1):
+		return None
+	if img == None:
+		print("error occurs in imageop.py:mask:")
+		print("img is None")
+		return None
+	m = [[1 for j in range(rang)] for i in range(rang)]
+	return general_filter(img, m, rang)
 
-def sharpen(img, rang):
-	pass
+def sharpen_filter(img, k = 1):
+	"""
+	unsharp masking
+	"""
+	row, col = img.size[0], img.size[1]
+	f_ = average_filter(img, 3)
+	resIMG = Image.new("L", (row, col))
+	for x in range(row):
+		for y in range(col):
+			resIMG.putpixel((x, y), int((k + 1) * int(img.getpixel((x, y))) - k * int(f_.getpixel((x, y)))))
+	return resIMG
 
-def mask(img, mask, rang):
-	pass
+def general_filter(img, mask, rang, regu = True):
+	if not (rang & 1):
+		return None
+	if img == None:
+		print("error occurs in imageop.py:mask:")
+		print("img is None")
+		return None
+	if len(mask) == 0 or len(mask) != rang or len(mask[0]) != rang:
+		print("error occurs in imageop.py:mask:")
+		print("the size of mask isn't right")
+		return None
 
-def static_mask(img, rang):
-	pass
+	offset = rang // 2
+	row, col = img.size[0], img.size[1]
+	resIMG = Image.new("L", (row, col))
+	val = [[0 for c in range(col)] for r in range(row)]
 
-def Laplacian_mask(img):
-	pass
+	for x in range(row):
+		for y in range(col):
+			val[x][y] = int(img.getpixel((x, y)))
+
+	for x in range(row):
+		for y in range(col):
+			newval = 0
+			Sum = 0
+			for s in range(-offset, offset + 1):
+				for t in range(-offset, offset + 1):
+					nx, ny = x + s, y + t
+					if nx < 0 or ny < 0 or nx >= row or ny >= col: continue
+					newval += mask[s + offset][t + offset] * val[nx][ny]
+					Sum += mask[s + offset][t + offset]
+			if Sum != 0 and regu:
+				newval //= Sum
+			resIMG.putpixel((x, y), newval)
+	return resIMG
+
+def median_filter(img, rang):
+	if img == None:
+		print("error occurs in imageop.py:mask:")
+		print("img is None")
+		return None
+	row, col = img.size[0], img.size[1]
+	offset = rang // 2
+	resIMG = Image.new("L", (row, col))
+	for x in range(row):
+		for y in range(col):
+			arr = []
+			for s in range(-offset, offset + 1):
+				for t in range(-offset, offset + 1):
+					nx, ny = x + s, y + t
+					if nx < 0 or ny < 0 or nx >= row or ny >= col: continue
+					arr.append(int(img.getpixel((nx, ny))))
+			l = len(arr)
+			arr.sort()
+			if l & 1:
+				resIMG.putpixel((x, y), arr[l // 2])
+			else:
+				resIMG.putpixel((x, y), (arr[l // 2] + arr[l // 2 - 1]) // 2)
+	return resIMG
+
+def Laplacian_filter(img):
+	if img == None:
+		print("error occurs in imageop.py:Laplacian_mask:")
+		print("img is None")
+		return None
+	row, col = img.size[0], img.size[1]
+	m = [[0,-1,0],
+		 [-1,4,-1],
+		 [0,-1,0]]
+	LM = general_filter(img, m, 3, False)
+	resIMG = Image.new("L", (row, col))
+	for x in range(row):
+		for y in range(col):
+			resIMG.putpixel((x, y), img.getpixel((x, y)) + LM.getpixel((x, y)))
+	return resIMG
--- hw2/test.py
+++ hw2/test.py
@@ -1,14 +1,28 @@
 import PIL
 import imageop
 import matplotlib.pyplot as plt
-img = PIL.Image.open("./testdata/elaine.512.tiff").convert("L")
+#img = PIL.Image.open("./testdata/elaine.512.tiff").convert("L")
 #img = PIL.Image.open("./testdata/lenna_gray.tif").convert("L")
+
+with open("./testdata/pirate_a.raw", 'rb') as f:
+	img = PIL.Image.frombytes("L", (512, 512), f.read(), 'raw')
+
 img.show()
-ohis = imageop.ravel(img)
-newimg = imageop.auto_level(img)
+#newimg = imageop.average_filter(img, 3)
+#newimg = imageop.general_filter(img, [[1,1,1],[1,1,1],[1,1,1]], 3)
+#newimg = imageop.extract_bit(img, i)
+#newimg = imageop.sharpen_filter(img, 1)
+newimg = imageop.median_filter(img, 3)
+#newimg = imageop.auto_level(img)
+#newimg.show()
+#newimg = imageop.Laplacian_filter(newimg)
 newimg.show()
+
+"""
+ohis = imageop.ravel(img)
 nhis = imageop.ravel(newimg)
 fig, axs = plt.subplots(1, 2)
 axs[0].hist(ohis, bins = 256, range = (0, 255))
 axs[1].hist(nhis, bins = 256, range = (0, 255))
 plt.show()
+"""
